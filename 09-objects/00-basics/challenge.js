8608fbf34d0d0ff6948ac1c0373398c05bc61809
eriksilver <eriksilver@users.noreply.github.com>
2015-02-19 09:01:13 -0600

    object exercises complete

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = { 
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents,
+  }
+  return course;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+ 
+  if (object.hasOwnProperty(newProp)) {
+    return object;
+  } else {
+     //object.newProp = newValue; not sure why dot syntax didnt work!
+     object[newProp] = newValue;
+     return object;
+  }
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender
 
-module.exports.canIGet = undefined;
+}
+
+module.exports.canIGet = function(item, money) {
+  var itemArray = ["MacBook Air", "MacBook Pro", "Mac Pro", "Apple Sticker"];
+  var itemPosition = itemArray.indexOf(item)
+
+  var priceTable = {
+    cost: [999, 1299, 2499, 1],
+    }
+  var test = money >= priceTable.cost[itemPosition]
+  return test
+
+};
